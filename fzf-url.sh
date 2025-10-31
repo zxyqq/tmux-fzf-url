83780811bc9d550792ce5fd5e610d196c9db994d
zhangxianyi <zhangxianyi@cestc.cn>
2025-10-31 15:34:54 +0800

    bash env

--- fzf-url.sh
+++ fzf-url.sh
@@ -1,2 +1,2 @@
-#!/bin/bash
+#!/usr/bin/env bash
 tmux capture-pane -J -p -S -99999 | grep -oE 'https?://[^[:space:]]+' | tac | awk '!seen[$0]++' | fzf --tmux --multi | while read url; do wslview "$url" & done
